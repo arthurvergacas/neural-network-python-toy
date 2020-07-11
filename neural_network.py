96e4639cfd366aaf80dcaee2a17a31336b05b64e
arthurvergacas <arthurvdm@gmail.com>
2020-07-11 11:40:35 -0300

    added predict and fixed training matrix operations

--- neural_network.py
+++ neural_network.py
@@ -1,10 +1,10 @@
 import numpy as np
-import random as rnd
+import random
 
 
 class NeuralNetwork:
     """
-    Neural Network Toy, by Arthur Vergaças. 
+    Neural Network Toy, by Arthur Vergaças.
     Based on Daniel Shiffman's library
     """
 
@@ -22,130 +22,181 @@ class NeuralNetwork:
         self.hidden_num = hidden
         self.outputs_num = outputs
 
-        self.weights_ih = np.full(
-            (self.hidden_num, self.inputs_num), rnd.random())
-        self.weights_ho = np.full(
-            (self.outputs_num, self.hidden_num), rnd.random())
+        self.weights_ih = np.random.rand(self.hidden_num, self.inputs_num)
+        self.weights_ho = np.random.rand(self.outputs_num, self.hidden_num)
 
-        self.bias_h = np.full((self.hidden_num, 1), rnd.random())
-        self.bias_o = np.full((self.outputs_num, 1), rnd.random())
+        self.bias_h = np.random.rand(self.hidden_num, 1)
+        self.bias_o = np.random.rand(self.outputs_num, 1)
 
         self.learning_rate = 0.1
 
-        def sigmoid(self, m):
-            """
-            Sigmoid function as an activation function.
-            Since it uses numpy bult-in functions, it is faster and apllied to all elements of the array.
+    def sigmoid(self, m):
+        """
+        Sigmoid function as an activation function.
+        Since it uses numpy bult-in functions, it is faster and apllied to all elements of the array.
 
 
-            Args:
-                x (NumPy array): the matrix that numpy will aplly the function
+        Args:
+            x (NumPy array): the matrix that numpy will aplly the function
 
-            Returns:
-                NumPy array: the matrix with the function applied to it
-            """
-            return 1 / (1 + np.exp(-m))
+        Returns:
+            NumPy array: the matrix with the function applied to it
+        """
+        return 1 / (1 + np.exp(-m))
 
-        def dsigmoid(self, m):
-            """
-            The derivative of the sigmoid function.
-            Used to determine how much the weights need to change.
-            To actually aplly it to all elements of the matrix, it is necessary to vectorize the function.
-            To do so, use np.vectorize(dsigmoid)(matrix)
+    def dsigmoid(self, m):
+        """
+        The derivative of the sigmoid function.
+        Used to determine how much the weights need to change.
+        To actually aplly it to all elements of the matrix, it is necessary to vectorize the function.
+        To do so, use np.vectorize(dsigmoid)(matrix)
 
-            Args:
-                m (NumPy Array): the matrix that the function will be apllied
+        Args:
+            m (NumPy Array): the matrix that the function will be apllied
 
-            Returns:
-                NumPy array: actually, to return a numpy array its necessary to 
-                first vectorize it using np.vectorize(dsigmoid)(matrix)
-            """
-            return m * (1 - m)
+        Returns:
+            NumPy array: actually, to return a numpy array its necessary to
+            first vectorize it using np.vectorize(dsigmoid)(matrix)
+        """
+        return m * (1 - m)
 
-        def predict(self, input_array):
-            """
-            Predicts the output of a certain input
+    def predict(self, input_array):
+        """
+        Predicts the output of a certain input
 
-            Args:
-                input_array (array): An array of inputs suited to the Neural Network training
-            """
+        Args:
+            input_array (array): An array of inputs suited to the Neural Network training
+        Returns:
+            returns a Python list with the guesses
+        """
 
-            # FEEDFORWARD ALGORITHM
+        # FEEDFORWARD ALGORITHM
+        # the algorithm is the same used in the train function
+        # just stored it here to have a way to predict values
 
-            pass
+        # create the numpy arrays
+        inputs = np.array(input_array, ndmin=2).T
 
-        def train(self, input_array, label_array):
-            """
-            Trains the neural network with supervised learning
-            The 'train' method trains the neural network just once, so it's necessary 
-            to train it within a loop
+        # FEEDFORWARD ALGORITHM
 
-            Args:
-                input_array (array): An array of inputs
-                label_array (array): An array of labes, according to the inputs
-            """
+        # define the hidden layer outputs
+        # input layer -> hidden layer
+        hidden_output = np.dot(self.weights_ih, inputs)
+        hidden_output = hidden_output + self.bias_h
+        # apply activation function
+        hidden_output = self.sigmoid(hidden_output)
 
-            # create the numpy arrays
-            inputs = np.array(input_array)
-            labels = np.array(label_array)
+        # define the output layer outputs
+        # hidden layer -> output layer
+        output_output = np.dot(self.weights_ho, hidden_output)
+        output_output = output_output + self.bias_o
+        # apply activation function
+        output_output = self.sigmoid(output_output)
 
-            # FEEDFORWARD ALGORITHM
+        output_output.tolist()
 
-            # define the hidden layer outputs
-            # input layer -> hidden layer
-            hidden_output = self.weights_ih.dot(inputs)
-            hidden_output += self.bias_h
-            # apply activation function
-            hidden_output = self.sigmoid(hidden_output)
+        return output_output
 
-            # define the output layer outputs
-            # hidden layer -> output layer
-            output_output = self.weights_ho.dot(hidden_output)
-            output_output += self.bias_o
-            # apply activation function
-            output_output = self.sigmoid(output_output)
+    def train(self, input_array, label_array):
+        """
+        Trains the neural network with supervised learning
+        The 'train' method trains the neural network just once, so it's necessary
+        to train it within a loop
 
-            # BACKPROPAGATION ALGORITHM
+        Args:
+            input_array (array): An array of inputs
+            label_array (array): An array of labes, according to the inputs
+        """
 
-            # calculate how much the network has missed
-            # ERROR = TARGET - OUTPUT
+        # create the numpy arrays
+        inputs = np.array(input_array, ndmin=2).T
+        labels = np.array(label_array, ndmin=2).T
 
-            # output layer error
-            output_error = labels - output_output
+        # FEEDFORWARD ALGORITHM
 
-            # calculate the gradient of the formula to backpropagate the error to the first layers
-            # hardcore math and calculus, might see later
-            # each layer has its own gradient, as well its own formula
-            # using vectorize for reasons described in the dsigmoid function
-            output_gradient = np.vectorize(self.dsigmoid)(output_output)
-            output_gradient = output_gradient.dot(output_error)
-            output_gradient *= self.learning_rate
+        # define the hidden layer outputs
+        # input layer -> hidden layer
+        hidden_output = np.dot(self.weights_ih, inputs)
+        hidden_output = hidden_output + self.bias_h
+        # apply activation function
+        hidden_output = self.sigmoid(hidden_output)
 
-            # calculate how much the weights must change
-            # with the transposed matrix, since it's going backwards
-            weights_ho_delta = output_gradient.dot(hidden_output.T)
+        # define the output layer outputs
+        # hidden layer -> output layer
+        output_output = np.dot(self.weights_ho, hidden_output)
+        output_output = output_output + self.bias_o
+        # apply activation function
+        output_output = self.sigmoid(output_output)
 
-            # adjust weights
-            self.weights_ho += weights_ho_delta
-            # adjust bias (its just the gradient)
-            self.bias_o += output_gradient
+        # BACKPROPAGATION ALGORITHM
 
-            # hidden layer error
-            hidden_error = self.weights_ho.T.dot(output_error)
+        # calculate how much the network has missed
+        # ERROR = TARGET - OUTPUT
 
-            # calculate hidden layer gradient (same proccess)
-            hidden_gradient = np.vectorize(self.dsigmoid)(hidden_output)
-            hidden_gradient = hidden_gradient.dot(hidden_error)
-            hidden_gradient *= self.learning_rate
+        # output layer error
+        output_error = labels - output_output
 
-            # hidden deltas
-            weights_ih_delta = hidden_gradient.dot(inputs.T)
+        # calculate the gradient of the formula to backpropagate the error to the first layers
+        # hardcore math and calculus, might see later
+        # each layer has its own gradient, as well its own formula
+        # using vectorize for reasons described in the dsigmoid function
+        output_gradient = np.vectorize(self.dsigmoid)(output_output)
+        output_gradient *= output_error
+        output_gradient *= self.learning_rate
 
-            # adjust weights
-            self.weights_ih += weights_ih_delta
-            # adjust bias
-            self.bias_h += hidden_gradient
+        # calculate how much the weights must change
+        # with the transposed matrix, since it's going backwards
+        weights_ho_delta = np.dot(output_gradient, hidden_output.T)
+
+        # adjust weights
+        self.weights_ho += weights_ho_delta
+        # adjust bias (its just the gradient)
+        self.bias_o += output_gradient
+
+        # hidden layer error
+        hidden_error = np.dot(self.weights_ho.T, output_error)
+
+        # calculate hidden layer gradient (same proccess)
+        hidden_gradient = np.vectorize(self.dsigmoid)(hidden_output)
+        hidden_gradient *= hidden_error
+        hidden_gradient *= self.learning_rate
+
+        # hidden deltas
+        weights_ih_delta = np.dot(hidden_gradient, inputs.T)
+
+        # adjust weights
+        self.weights_ih += weights_ih_delta
+        # adjust bias
+        self.bias_h += hidden_gradient
 
 
 if __name__ == "__main__":
-    clss = NeuralNetwork(2, 3, 1)
+    clss = NeuralNetwork(2, 4, 1)
+
+    data_set = [
+        {
+            "input": [0, 0],
+            "label": [0]
+        },
+        {
+            "input": [1, 0],
+            "label": [1]
+        },
+        {
+            "input": [0, 1],
+            "label": [1]
+        },
+        {
+            "input": [1, 1],
+            "label": [0]
+        },
+    ]
+
+    for i in range(50000):
+        current = random.choice(data_set)
+        clss.train(current["input"], current["label"])
+
+    print(clss.predict([1, 1]))
+    print(clss.predict([0, 1]))
+    print(clss.predict([1, 0]))
+    print(clss.predict([0, 0]))
